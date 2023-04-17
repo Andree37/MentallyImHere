55f12f6a45b8c765795b9bbb67c0333c388c0aa9
Andre Ribeiro <andresmribeiro@gmail.com>
2023-04-17 23:54:28 +0100

    chore: split the sections into components

---
+++ src/components/Faqs.tsx
@@ -0,0 +1,144 @@
+export default function Faqs() {
+    return (
+        <section id="faqs" className="my-32">
+            <div className="mx-auto px-4 sm:px-12 xl:max-w-5xl xl:px-0">
+                <h2 className="mb-12 text-center text-3xl font-bold text-gray-800 dark:text-white md:text-4xl xl:text-5xl">Frequently
+                    Asqued Questions</h2>
+
+                <div
+                    className="divide-y divide-gray-200 border-y border-gray-200 dark:divide-gray-800 dark:border-gray-800">
+                    <div>
+                        <dl className="faq mx-auto max-w-2xl">
+                            <dt className="text-lg">
+                                <button type="button"
+                                        className="flex w-full items-start justify-between py-6 text-left text-gray-400"
+                                        aria-controls="faq-0" data-active="false">
+                                    <span className="font-medium text-gray-900 dark:text-white">What included in the pack ?</span>
+                                    <span className="ml-6 flex h-7 items-center">
+                                        <svg className="arrow-down h-6 w-6 rotate-0 transform duration-300"
+                                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                             strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
+                                        </svg>
+                                    </span>
+                                </button>
+                            </dt>
+                            <dd className="faq-answer block max-h-0 overflow-hidden pr-12 duration-300 ease-in-out"
+                                id="faq-0">
+                                <p className="pb-6 text-base text-gray-600 dark:text-gray-400">Lorem ipsum, dolor
+                                    sit amet consectetur adipisicing elit. Laborum quod pariatur possimus at fugit
+                                    natus aspernatur molestiae. Velit, odio modi provident necessitatibus molestias
+                                    qui voluptatibus similique magnam a nam rem!</p>
+                            </dd>
+                        </dl>
+                    </div>
+                    <div>
+                        <dl className="faq mx-auto max-w-2xl">
+                            <dt className="text-lg">
+                                <button type="button"
+                                        className="flex w-full items-start justify-between py-6 text-left text-gray-400"
+                                        aria-controls="faq-1" data-active="false">
+                                    <span className="font-medium text-gray-900 dark:text-white">What included in the pack ?</span>
+                                    <span className="ml-6 flex h-7 items-center">
+                                        <svg className="arrow-down h-6 w-6 rotate-0 transform duration-300"
+                                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                             strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
+                                        </svg>
+                                    </span>
+                                </button>
+                            </dt>
+                            <dd className="faq-answer block max-h-0 overflow-hidden pr-12 duration-300 ease-in-out"
+                                id="faq-1">
+                                <p className="pb-6 text-base text-gray-600 dark:text-gray-400">Lorem ipsum, dolor
+                                    sit amet consectetur adipisicing elit. Laborum quod pariatur possimus at fugit
+                                    natus aspernatur molestiae. Velit, odio modi provident necessitatibus molestias
+                                    qui voluptatibus similique magnam a nam rem!</p>
+                            </dd>
+                        </dl>
+                    </div>
+                    <div>
+                        <dl className="faq mx-auto max-w-2xl">
+                            <dt className="text-lg">
+                                <button type="button"
+                                        className="flex w-full items-start justify-between py-6 text-left text-gray-400"
+                                        aria-controls="faq-2" data-active="false">
+                                    <span className="font-medium text-gray-900 dark:text-white">What included in the pack ?</span>
+                                    <span className="ml-6 flex h-7 items-center">
+                                        <svg className="arrow-down h-6 w-6 rotate-0 transform duration-300"
+                                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                             strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
+                                        </svg>
+                                    </span>
+                                </button>
+                            </dt>
+                            <dd className="faq-answer block max-h-0 overflow-hidden pr-12 duration-300 ease-in-out"
+                                id="faq-2">
+                                <p className="pb-6 text-base text-gray-600 dark:text-gray-400">Lorem ipsum, dolor
+                                    sit amet consectetur adipisicing elit. Laborum quod pariatur possimus at fugit
+                                    natus aspernatur molestiae. Velit, odio modi provident necessitatibus molestias
+                                    qui voluptatibus similique magnam a nam rem!</p>
+                            </dd>
+                        </dl>
+                    </div>
+                    <div>
+                        <dl className="faq mx-auto max-w-2xl">
+                            <dt className="text-lg">
+                                <button type="button"
+                                        className="flex w-full items-start justify-between py-6 text-left text-gray-400"
+                                        aria-controls="faq-4" data-active="false">
+                                    <span className="font-medium text-gray-900 dark:text-white">Are updates included in this pack ?</span>
+                                    <span className="ml-6 flex h-7 items-center">
+                                        <svg className="arrow-down h-6 w-6 rotate-0 transform duration-300"
+                                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                             strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
+                                        </svg>
+                                    </span>
+                                </button>
+                            </dt>
+                            <dd className="faq-answer block max-h-0 overflow-hidden pr-12 duration-300 ease-in-out"
+                                id="faq-4">
+                                <p className="pb-6 text-base text-gray-600 dark:text-gray-400">Lorem ipsum, dolor
+                                    sit amet consectetur adipisicing elit. Laborum quod pariatur possimus at fugit
+                                    natus aspernatur molestiae. Velit, odio modi provident necessitatibus molestias
+                                    qui voluptatibus similique magnam a nam rem!</p>
+                            </dd>
+                        </dl>
+                    </div>
+                    <div>
+                        <dl className="faq mx-auto max-w-2xl">
+                            <dt className="text-lg">
+                                <button type="button"
+                                        className="flex w-full items-start justify-between py-6 text-left text-gray-400"
+                                        aria-controls="faq-5" data-active="false">
+                                    <span className="font-medium text-gray-900 dark:text-white">What included in the pack ?</span>
+                                    <span className="ml-6 flex h-7 items-center">
+                                        <svg className="arrow-down h-6 w-6 rotate-0 transform duration-300"
+                                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                             strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
+                                        </svg>
+                                    </span>
+                                </button>
+                            </dt>
+                            <dd className="faq-answer block max-h-0 overflow-hidden pr-12 duration-300 ease-in-out"
+                                id="faq-5">
+                                <p className="pb-6 text-base text-gray-600 dark:text-gray-400">Lorem ipsum, dolor
+                                    sit amet consectetur adipisicing elit. Laborum quod pariatur possimus at fugit
+                                    natus aspernatur molestiae. Velit, odio modi provident necessitatibus molestias
+                                    qui voluptatibus similique magnam a nam rem!</p>
+                            </dd>
+                        </dl>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}

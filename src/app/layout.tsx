01c32549fa3ab2b683e3d6ae75550bad67ccf686
Tomás SANTOS <tommasini15@gmail.com>
2023-08-08 03:20:46 +0100

    implement google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import './globals.css'
+import "./globals.css";
 import React from "react";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export const metadata = {
-    title: 'Genipsi',
-    description: 'Genipsi - Mental Health Platform',
-}
+  title: "Genipsi",
+  description: "Genipsi - Mental Health Platform",
+};
 
 export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-        <body>
-        <Navbar/>
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
         {children}
-        <Footer/>
-        </body>
-        </html>
-    )
+        <Footer />
+      </body>
+    </html>
+  );
 }

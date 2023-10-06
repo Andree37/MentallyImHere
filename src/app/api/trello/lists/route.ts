f7c38cf5d7a40b92d4be7d8fc0cfc3deb81432cb
Andre Ribeiro <andresmribeiro@gmail.com>
2023-10-06 15:23:16 +0100

    mby?

--- src/app/api/trello/lists/route.ts
+++ src/app/api/trello/lists/route.ts
@@ -1,6 +1,6 @@
 import {NextResponse} from "next/server";
 
-export async function GET() {
+export async function GET(req: Request) {
     const apiKey = process.env.TRELLO_KEY;
     const trelloToken = process.env.TRELLO_TOKEN;
     const boardID = 'Oflyc2ub';

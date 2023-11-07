50f23b65ae72202f18fa4885d3a3733d3088574a
Andre Ribeiro <andresmribeiro@gmail.com>
2023-11-07 21:19:37 +0000

    psiregister

--- src/components/JoinUs.tsx
+++ src/components/JoinUs.tsx
@@ -45,7 +45,7 @@ export function JoinUs() {
                     </p>
                     <div className="mt-8">
                         <Link
-                            href="/professional"
+                            href="/psiregister"
                             className="inline-flex rounded-md bg-white/10 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-white/20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                         >
                             Inscreva-se na nossa plataforma

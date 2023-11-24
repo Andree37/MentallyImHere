4090a88df206e714faf7b9d2fecb1efb64f7a92f
Andre Ribeiro <andresmribeiro@gmail.com>
2023-11-24 23:28:18 +0000

    feat: email as default for info form

--- src/components/InfoForm.tsx
+++ src/components/InfoForm.tsx
@@ -64,7 +64,7 @@ type UserFormValues = z.infer<typeof userFormSchema>;
 // This can come from your database or API.
 const defaultValues: Partial<UserFormValues> = {
     name: '',
-    contactFrom: 'Chamada',
+    contactFrom: 'Email',
     consultLocation: 'Online',
     phone: '',
     motivation: '',

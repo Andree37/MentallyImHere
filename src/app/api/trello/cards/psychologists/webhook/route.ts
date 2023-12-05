15db9eae3bc06a072ee4faa6b6bdf2071b27bb45
Tomás SANTOS <tommasini15@gmail.com>
2023-12-05 23:44:44 +0000

    psi webhook

---
+++ src/app/api/trello/cards/psychologists/webhook/route.ts
@@ -0,0 +1,37 @@
+import clientPromise from '@/lib/mongo';
+
+interface trelloWebhook {
+    action: { data: { list: { id: string }; card: { id: string } } };
+}
+
+export async function POST(_: Request, response: Response) {
+    const res = (await response.json()) as trelloWebhook;
+    const listId = res.action.data.list.id;
+
+    const acceptedPsysListId = '654b64f691952533bd65241d';
+
+    const cardId = res.action.data.card.id;
+
+    const cardResponse = await fetch(`https://api.trello.com/1/cards/${cardId}?key=APIKey&token=APIToken`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+        },
+    });
+    if (!cardResponse.ok) {
+        return;
+    }
+
+    const card = await cardResponse.json();
+    const regex = /Cédula OPP:(\d+)/;
+    const oppCedule = regex.exec(card.desc);
+
+    const client = await clientPromise;
+    const db = client.db('Genipsi');
+
+    if (acceptedPsysListId === listId) {
+        await db.collection('psi').updateOne({ opp: oppCedule }, { $set: { approved: true } });
+    } else {
+        await db.collection('psi').updateOne({ opp: oppCedule }, { $set: { approved: false } });
+    }
+}

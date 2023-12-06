c5dd336ec85d3404cc23fc4ca4f9c857f87dd14d
Tomás SANTOS <tommasini15@gmail.com>
2023-12-06 00:44:26 +0000

    add email validation

--- src/app/api/trello/cards/psychologists/webhook/route.ts
+++ src/app/api/trello/cards/psychologists/webhook/route.ts
@@ -4,6 +4,30 @@ interface trelloWebhook {
     action: { data: { listAfter: { id: string }; card: { id: string } } };
 }
 
+function extractEmail(inputString: string) {
+    const emailPrefix = 'Email: ';
+
+    // Find the index of "Email: "
+    const emailIndex = inputString.indexOf(emailPrefix);
+
+    // Check if "Email: " was found
+    if (emailIndex !== -1) {
+        // Extract the email substring
+        const emailSubstring = inputString.substring(emailIndex + emailPrefix.length);
+
+        // Find the end of the email (assuming it ends with a line break)
+        const lineBreakIndex = emailSubstring.indexOf('\n');
+
+        // Extract the email
+        const email = lineBreakIndex !== -1 ? emailSubstring.substring(0, lineBreakIndex) : emailSubstring;
+
+        return email.trim(); // Trim to remove leading/trailing whitespaces
+    } else {
+        // Return null if "Email: " was not found
+        return null;
+    }
+}
+
 export async function POST(req: Request, response: Response) {
     const apiKey = process.env.TRELLO_KEY;
     const trelloToken = process.env.TRELLO_TOKEN;
@@ -30,12 +54,14 @@ export async function POST(req: Request, response: Response) {
     if (!match) return;
     const oppCedule = match[1];
 
+    const email = extractEmail(card.desc);
+
     const client = await clientPromise;
     const db = client.db('Genipsi');
 
     if (acceptedPsysListId === listId) {
-        await db.collection('psi').updateOne({ opp: oppCedule }, { $set: { approved: true } });
+        await db.collection('psi').updateOne({ opp: oppCedule, email }, { $set: { approved: true } });
     } else {
-        await db.collection('psi').updateOne({ opp: oppCedule }, { $set: { approved: false } });
+        await db.collection('psi').updateOne({ opp: oppCedule, email }, { $set: { approved: false } });
     }
 }

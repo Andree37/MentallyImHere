c1b0bd0daf32f5f6452f42e0101ac864d08d8346
Andre Ribeiro <andresmribeiro@gmail.com>
2023-12-21 20:40:33 +0000

    feat: api for clients with and without consultations

---
+++ src/app/api/advertisers/[id]/converted/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server';
+import clientPromise from '@/lib/mongo';
+
+export async function GET(req: NextRequest) {
+    const client = await clientPromise;
+    const db = client.db('Genipsi');
+
+    const searchParams = req.nextUrl.searchParams;
+    const adv_id = searchParams.get('id');
+    const filter: any = adv_id && adv_id !== 'null' ? { advertiserID: adv_id } : {};
+
+    filter['found_consultation'] = true;
+
+    const data = await db.collection('clients-quill').findOne(filter);
+
+    return NextResponse.json({ data });
+}

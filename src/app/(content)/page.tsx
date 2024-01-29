8739572b0875d3f57b928d195e1a7e9ed01b2b83
Andre Ribeiro <andresmribeiro@gmail.com>
2024-01-29 13:24:08 +0000

    upgrade: nextjs version bump 14

--- src/app/(content)/page.tsx
+++ src/app/(content)/page.tsx
@@ -1,9 +1,9 @@
 import Hero from '@/components/Hero';
-import EmailCTA from '@/components/EmailCTA';
 import Faqs from '@/components/Faqs';
 import Solution from '@/components/Solution';
-import GoogleAnalytics from '@/components/GoogleAnalytics';
+import EmailCTA from '@/components/EmailCTA';
 import { JoinUs } from '@/components/JoinUs';
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 
 const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
 

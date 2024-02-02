03850da384bc6d25820860f22cf162a69cdd84d0
Andre Ribeiro <andresmribeiro@gmail.com>
2024-02-02 16:49:43 +0000

    mongopg some more mapping

--- csv_data/mongres.js
+++ csv_data/mongres.js
@@ -2,7 +2,6 @@ const { Pool } = require('pg');
 const csv = require('csv-parser');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-const { data } = require('autoprefixer');
 require('dotenv').config();
 
 const pool = new Pool({
@@ -40,65 +39,105 @@ function readCsv(filePath, onData) {
 }
 
 async function loadClients() {
-    const clients = await readCsv('clients.csv', (data) => ({
-        id: uuidv4(),
-        age: data.age,
-        email: data.email,
-        // ... other fields ...
-    }));
-    console.log(clients);
+    const clients = await readCsv('clients.csv', (data) => {
+        console.log(data);
+
+        return {
+            id: uuidv4(),
+            name: '',
+            gender: '',
+            age: 0,
+            email: '',
+            location_district: '',
+            location_municipality: '',
+            contact_preference: '',
+            contact_preference_phone: '',
+            // other fields for client requests
+            frequency: '',
+            price: 0,
+            consultation_for: '',
+            motivation: '',
+            had_experience_therapy: '',
+            describe_experience_therapy: '',
+            immediate_availability: false,
+            availability_describe: '',
+            other_availability: '',
+            preferential_consultation_type: '[]',
+            professional_gender: '[]',
+            advertiserID: null,
+        };
+    });
+    console.log(clients.length);
     //await insertIntoPg('clients', clients);
 }
 
-/*
-{
-  'preferential-consultation-type': [ 'mixed' ],
-  'professional-gender': [ 'no-preference' ],
-}
- */
 async function loadClientsQuill() {
     const clients = await readCsv('clients_quill.csv', (data) => {
-        console.log(`data: ${data.data}`);
         const pData = parseDataString(data.data);
-        console.log(`pData: ${JSON.stringify(pData)}`);
         if (Object.keys(pData).length === 0) return;
+
         return {
             id: uuidv4(),
             name: pData.name,
             gender: pData['auto-describe-gender'] || pData.gender[0],
             age: pData.age,
             email: pData.email,
-            location_district: pData.location, // check this after
-            location_municipality: pData.location, // check this after
+            location_district: pData.location, // check this after to pass to district
+            location_municipality: pData.location, // check this after to pass to municipality
             contact_preference: pData['contact-preference'] ? pData['contact-preference'][0] : 'phone',
             contact_preference_phone: pData['contact-preference-phone'],
             // other fields for client requests
             frequency: pData['frequency-for-other'] || pData.frequency[0],
             price: pData.price,
-            consultation_for: pData['consultation-for-other'] || pData['consultation-for'][0],
+            consultation_for:
+                pData['consultation-for-other'] || (pData['consultation-for'] && pData['consultation-for'][0]) || null,
             motivation: pData.motivation,
-            had_experience_therapy: pData['previous-experience-therapy'][0] === 'yes',
+            had_experience_therapy:
+                (pData['previous-experience-therapy'] && pData['previous-experience-therapy'][0] === 'yes') || false,
             describe_experience_therapy: pData['prev-experience-yes'],
-            immediate_availability: pData['immediate-availability'][0] === 'immediate',
+            immediate_availability:
+                (pData['immediate-availability'] && pData['immediate-availability'][0] === 'immediate') || false,
             availability_describe: pData['availability-describe'],
-            when_availability: pData['immediate-availability-other'],
+            other_availability: pData['immediate-availability-other'],
             preferential_consultation_type: pData['preferential-consultation-type'], // this is an array, fix it after
             professional_gender: pData['professional-gender'], // this is an array, fix it after
             advertiserID: null,
         };
     });
 
+    console.log(clients.length);
+
     //await insertIntoPg('clients', clients);
 }
 
 async function loadPsis() {
-    const psis = await readCsv('psi.csv', (data) => ({
-        id: uuidv4(),
-        age: data.age,
-        email: data.email,
-        // ... other fields ...
-    }));
-    console.log(psis);
+    const psis = await readCsv('psi.csv', (data) => {
+        if (!data.cost) {
+            console.error('Something iffy:', data);
+            return null;
+        }
+        const costs = JSON.parse(data.cost);
+
+        return {
+            id: uuidv4(),
+            gender: data.gender,
+            age: +data.age,
+            specialization: data.specialization,
+            phone: data.phone,
+            email: data.email,
+            location_district: data.location, // check this after to pass to district
+            location_municipality: data.location, // check this after to pass to municipality
+            experience_years: +data.experienceYears,
+            consultation_type: data.consultationType,
+            availability: data.availability,
+            cost_from: costs[0],
+            cost_to: costs[1],
+            opp: data.opp,
+            approved: false,
+            preferred_fee_type: '',
+        };
+    });
+    console.log(psis.length);
     // await insertIntoPg('psis', psis);
 }
 
@@ -149,9 +188,9 @@ function parseDataString(dataString) {
 
 async function run() {
     try {
-        // await loadClients();
+        await loadClients();
         await loadClientsQuill();
-        // await loadPsis();
+        await loadPsis();
     } catch (e) {
         console.error(e);
     } finally {
@@ -159,4 +198,6 @@ async function run() {
     }
 }
 
-run();
+run()
+    .then(() => console.log('Done'))
+    .catch((e) => console.error(e));
